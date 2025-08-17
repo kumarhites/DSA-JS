70a65feb825bdb0f2a598a2ba38d41a0bc15c6f3
Hitesh <hkhiteshkumar66@gmail.com>
2025-08-17 12:01:05 +0530

    removeElement.js

--- code/removeElements.js
+++ code/removeElements.js
@@ -1,9 +1,28 @@
-function removeElements(nums, val) {
-  let i = 0;
-  for (let j = 1; j < nums.length; j++) {}
-}
+// function removeElements(nums, val) {
+//   let i = 0;
+//   let newArr = [];
+//   for (let j = 0; j < nums.length; j++) {
+//     if (nums[j] !== val) {
+//       newArr[i] = nums[j];
+//       i++;
+//     }
+//   }
+//   return newArr.length;
+// }
 
-console.log(removeElements([1, 2, 3, 4, 5, 5, 5, 4, 3, 3, 2], 2));
+// console.log(removeElements([1, 2, 3, 4, 5, 5, 5, 4, 3, 3, 2], 2));
 
 // module.exports = removeElements;
-4;
+
+function removeElements(nums, val) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+  return i;
+}
+console.log(removeElements([1, 2, 3, 4, 5, 5, 5, 4, 3, 3, 2], 2));
+module.exports = removeElements;
